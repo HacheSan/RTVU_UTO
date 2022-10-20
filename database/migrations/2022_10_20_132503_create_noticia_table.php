50778014f89b5ab087a5e2bacad867157ab13e59
hachesan <chinosanchezhermogenes@gmail.com>
2022-10-20 10:19:58 -0400

    creacion de tablas de BdD

---
+++ database/migrations/2022_10_20_132503_create_noticia_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('noticia', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('categoria_noticia',50);
+            $table->string('titulo',250);
+            $table->text('descripcion');
+            $table->string('foto_noticia',150);
+            $table->string('video_noticia',150);
+            $table->string('ciudad',25);
+            $table->string('pais',25);
+            $table->date('fecha_registro');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('noticia');
+    }
+};
---
+++ database/migrations/2022_10_20_132908_create_empresa_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('empresa', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre_empresa',50);
+            $table->text('descripcion');
+            $table->string('foto_empresa',150);
+            $table->text('mision');
+            $table->text('vision');
+            $table->string('direccion',150);
+            $table->integer('telefono',15)->unsigned();
+            $table->string('email');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('empresa');
+    }
+};
